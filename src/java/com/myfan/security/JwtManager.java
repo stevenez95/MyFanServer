d7baabe536aabf0a0a04a4a398b054c686c1f219
Steven <Steven@StevenJM>
2016-05-18 22:57:10 -0600

    Steven: Se agreo las clases para DTO, seguridad JWT, conexion a Twitter

---
+++ src/java/com/myfan/security/JwtManager.java
@@ -0,0 +1,87 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package com.myfan.security;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.List;
+import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
+import org.jose4j.keys.HmacKey;
+import org.jose4j.lang.JoseException;
+
+/**
+ *
+ * @author steve_000
+ */
+public class JwtManager {
+    
+    private final String key="asdfghjjsdfsdfsdfsdfsdfsdfqwerty";
+    private Key k = new HmacKey(key.getBytes());
+    
+    public JwtManager() {
+    }
+    
+    public String jwtGenerate(){      
+        JwtClaims claims = new JwtClaims();
+        claims.setIssuer("Issuer");  // who creates the token and signs it
+        claims.setAudience("Audience"); // to whom the token is intended to be sent
+        claims.setExpirationTimeMinutesInTheFuture(10); // time when the token will expire (10 minutes from now)
+        claims.setGeneratedJwtId(); // a unique identifier for the token
+        claims.setIssuedAtToNow();  // when the token was issued/created (now)
+        claims.setNotBeforeMinutesInThePast(2); // time before which the token is not yet valid (2 minutes ago)
+        claims.setSubject("subject"); // the subject/principal is whom the token is about
+        claims.setClaim("email","mail@example.com"); // additional claims/attributes about the subject can be added
+        List<String> groups = Arrays.asList("group-one", "other-group", "group-three");
+        claims.setStringListClaim("groups", groups); // multi-valued claims work too and will end up as a JSON array
+        
+        // A JWT is a JWS and/or a JWE with JSON claims as the payload.
+        // In this example it is a JWS so we create a JsonWebSignature object.
+        JsonWebSignature jws = new JsonWebSignature();
+        
+        // The payload of the JWS is JSON content of the JWT Claims
+        jws.setPayload(claims.toJson());
+        
+        // The JWT is signed using the private key
+        jws.setKey(k);
+        
+        
+        // Set the signature algorithm on the JWT/JWS that will integrity protect the claims
+        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.HMAC_SHA256);
+        
+        String jwt="";
+        try {
+            jwt = "\""+jws.getCompactSerialization()+"\"";
+        } catch (JoseException ex) {
+            return "";
+        }
+        
+        return jwt;
+    }
+    
+    public boolean jwtValidate(String jwt){
+        JwtConsumer jwtConsumer = new JwtConsumerBuilder()
+                .setRequireExpirationTime() // the JWT must have an expiration time
+                .setAllowedClockSkewInSeconds(30) // allow some leeway in validating time based claims to account for clock skew
+                .setRequireSubject() // the JWT must have a subject claim
+                .setExpectedIssuer("Issuer") // whom the JWT needs to have been issued by
+                .setExpectedAudience("Audience") // to whom the JWT is intended for
+                .setVerificationKey(k) // verify the signature with the public key
+                .build(); // create the JwtConsumer instance
+        JwtClaims jwtClaims=null;
+        try {
+            jwtClaims = jwtConsumer.processToClaims(jwt);
+            return true;
+        } catch (InvalidJwtException ex) {
+            return false;
+        }
+    }
+    
+}
---
+++ src/java/com/myfan/services/MyFanService.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myfan.services;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+/**
+ * REST Web Service
+ *
+ * @author Steven
+ */
+@Path("myfan")
+public class MyFanService {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of WebResource
+     */
+    public MyFanService() {
+    }
+
+    /**
+     * Retrieves representation of an instance of com.mymusic.services.WebResource
+     * @param token
+     * @param id
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getJson(@HeaderParam("x-access-token") String token,@PathParam("id") int id) {
+        //TODO return proper representation object
+        System.out.println("ID: "+id);
+        return Response.ok("{\"token\":\""+token+"\"}", MediaType.APPLICATION_JSON).build();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of WebResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putJson(String content) {
+    }
+}
