6aa3b8a4914338da22d2a9de29ccd3c1c83a4cbc
Steven <Steven@StevenJM>
2016-05-26 17:37:25 -0600

    Steven: Cambions DTO

---
+++ src/java/com/myfan/dto/ResenaEvento.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myfan.dto;
+
+/**
+ *
+ * @author Steven
+ */
+public class ResenaEvento {
+    
+    private int idFan, idEvento, calificacion;
+    private String comentario;
+
+    /**
+     * @return the idFan
+     */
+    public int getIdFan() {
+        return idFan;
+    }
+
+    /**
+     * @param idFan the idFan to set
+     */
+    public void setIdFan(int idFan) {
+        this.idFan = idFan;
+    }
+
+    /**
+     * @return the idEvento
+     */
+    public int getIdEvento() {
+        return idEvento;
+    }
+
+    /**
+     * @param idEvento the idEvento to set
+     */
+    public void setIdEvento(int idEvento) {
+        this.idEvento = idEvento;
+    }
+
+    /**
+     * @return the calificacion
+     */
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    /**
+     * @param calificacion the calificacion to set
+     */
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    /**
+     * @return the comentario
+     */
+    public String getComentario() {
+        return comentario;
+    }
+
+    /**
+     * @param comentario the comentario to set
+     */
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+    
+}
