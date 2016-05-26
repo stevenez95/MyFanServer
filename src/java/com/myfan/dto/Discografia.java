ce92186568ace948954b2b0fbc95fb44c0b5a340
franr <franr@DESKTOP-HAMBQGS>
2016-05-26 17:38:17 -0600

    Se crearon los argumentos de las clases dto

--- src/java/com/myfan/dto/Discografia.java
+++ src/java/com/myfan/dto/Discografia.java
@@ -10,5 +10,108 @@ package com.myfan.dto;
  * @author Steven
  */
 public class Discografia {
+    private int idDiscografia,anioPublicacion,idBanda;
+    private String nombre,descripcion,generoMusical,selloDiscografico;
+
+    /**
+     * @return the idDiscografia
+     */
+    public int getIdDiscografia() {
+        return idDiscografia;
+    }
+
+    /**
+     * @param idDiscografia the idDiscografia to set
+     */
+    public void setIdDiscografia(int idDiscografia) {
+        this.idDiscografia = idDiscografia;
+    }
+
+    /**
+     * @return the anioPublicacion
+     */
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    /**
+     * @param anioPublicacion the anioPublicacion to set
+     */
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    /**
+     * @return the idBanda
+     */
+    public int getIdBanda() {
+        return idBanda;
+    }
+
+    /**
+     * @param idBanda the idBanda to set
+     */
+    public void setIdBanda(int idBanda) {
+        this.idBanda = idBanda;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the generoMusical
+     */
+    public String getGeneroMusical() {
+        return generoMusical;
+    }
+
+    /**
+     * @param generoMusical the generoMusical to set
+     */
+    public void setGeneroMusical(String generoMusical) {
+        this.generoMusical = generoMusical;
+    }
+
+    /**
+     * @return the selloDiscografico
+     */
+    public String getSelloDiscografico() {
+        return selloDiscografico;
+    }
+
+    /**
+     * @param selloDiscografico the selloDiscografico to set
+     */
+    public void setSelloDiscografico(String selloDiscografico) {
+        this.selloDiscografico = selloDiscografico;
+    }
+    
+    
+    
     
 }
